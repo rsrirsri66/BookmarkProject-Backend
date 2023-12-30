12ce13d1b2549bc7dc0e5ecedd130ccb9c8f622f
rsrirsri66 <rsrirsri66@gmail.com>
2023-12-30 14:20:44 +0530

    update commit

--- app/controller/bookmarksController.js
+++ app/controller/bookmarksController.js
@@ -2,26 +2,69 @@ const db = require('../../config/db');
 
 exports.checkbooks = async (req, res) => {
 
-    const result = await db.query('SELECT * FROM bookmarks');
+  try {
+    const result = await db.query(`
+      SELECT b.id, b.title, b.description, b.url, STRING_AGG(t.title, ',') AS tag_title
+      FROM bookmarks b
+      LEFT JOIN bookmarks_tags bt ON b.id = bt.bookmark_id
+      LEFT JOIN tags t ON bt.tag_id = t.id
+      WHERE b.is_active = true
+      GROUP BY b.id, b.title, b.description, b.url
+      ORDER BY b.id DESC
+     
+    `);
+
     res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 
 };
 
-exports.createBookmark = async(req,res)=>{
-    const {url, title, description}=req.body;
-    try{
-        const result=await db.query(
-            'insert into bookmarks (url, title, description)values ($1, $2, $3)returning *',
-            [url, title, description]
-        );
-        const newBookmark = result.rows[0];
+exports.createBookmark = async (req, res) => {
+  const { url, title, description, tags } = req.body;
+  try {
+    // Insert bookmark into bookmarks table
+    const bookmarkResult = await db.query(
+      'INSERT INTO bookmarks (url, title, description) VALUES ($1, $2, $3) RETURNING *',
+      [url, title, description]
+    );
 
-        return res.status(201).json({ message: 'Bookmark added successfully', bookmark: newBookmark });
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
+    const newBookmark = bookmarkResult.rows[0];
+    const bookmarkId = newBookmark.id;
+
+    // Insert tags and associate with the bookmark in bookmarks_tags table
+    if (tags && tags.length > 0) {
+      const tagIds = await Promise.all(
+        tags.map(async (tagTitle) => {
+          // Check if tag exists, if not, insert it
+          const existingTag = await db.query('SELECT * FROM tags WHERE title = $1', [tagTitle]);
+          if (existingTag.rows.length === 0) {
+            const newTagResult = await db.query('INSERT INTO tags (title) VALUES ($1) RETURNING id', [tagTitle]);
+            return newTagResult.rows[0].id;
+          } else {
+            return existingTag.rows[0].id;
+          }
+        })
+      );
+
+      // Associate tags with the bookmark in bookmarks_tags table
+      await Promise.all(
+        tagIds.map(async (tagId) => {
+          
+          await db.query('INSERT INTO bookmarks_tags (bookmark_id, tag_id) VALUES ($1, $2)', [bookmarkId, tagId]);
+        })
+      );
     }
+
+    return res.status(201).json({ message: 'Bookmark added successfully', bookmark: newBookmark });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
 
  exports.updateBookmark = async (req, res) => {
         const { id } = req.params; // Assuming your route includes a parameter for the bookmark ID
